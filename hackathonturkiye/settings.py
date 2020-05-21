4db2a40d39466f3ba1f0432ab95bcb4fdc5ef913
divisia <omrfyyz@gmail.com>
2020-05-21 23:25:11 +0300

    requirements.txt

--- hackathonturkiye/settings.py
+++ hackathonturkiye/settings.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-SECRET_KEY = open("secret", "rt").read()
+SECRET_KEY = os.getenv("DJANGO_SECRET", open("secret", "rt").read())
 
 DEBUG = True
 
