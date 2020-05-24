a0def2c729c19e5c61a42088cd48579cfe43b8af
divisia <omrfyyz@gmail.com>
2020-05-25 01:36:18 +0300

    app: record app added

--- record/models.py
+++ record/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from event.models import Event
 
-# Create your models here.
+
+class AbstractRecord(models.Model):
+    """
+    Abstract class for recording usage statistics.
+    Not recommended to be used directly.
+    """
+
+    remote_addr = models.CharField(max_length=15)
+    path = models.CharField(max_length=128)
+    query_string = models.CharField(max_length=1024)
+    user_agent = models.CharField(max_length=1024)
+    method = models.CharField(max_length=5)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, editable=False)
+    debug = models.BooleanField(default=False)
+
+
+class EventRecord(Abstract):
+    """
+    Used for event tracking
+    """
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+
