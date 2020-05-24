4242ae987280a6041836beed38c51072df667572
divisia <omrfyyz@gmail.com>
2020-05-24 11:59:33 +0300

    switching to dev

--- event/views.py
+++ event/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render, HttpResponse
 from django.views import View
 from rest_framework import viewsets
-from event.models import Event
+
+from event.models import Event, EventType
 from event.serializers import *
 
 
@@ -11,6 +11,23 @@ class EventViewSet(viewsets.ModelViewSet):
     """
     serializer_class = EventSerializer
     queryset = Event.objects.filter(published=True).order_by('-added_at')
+    
+    def get_queryset(self):
+        params = self.request.query_params
+        print("request params", params)
+
+        etype = params.get('etype', None)
+        if etype:
+            print("filtering for event type", etype)
+            etype = EventType.objects.get(name=etype)
+            self.queryset = self.queryset.filter(etype=etype)
+        
+        after = params.get('after', None)
+        if after:
+            print("selecting events after", after)
+            self.queryset = self.queryset.where('starts_at__grea', after)
+
+        return self.queryset
 
 
 class EventTypeViewSet(viewsets.ModelViewSet):
