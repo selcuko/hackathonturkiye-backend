289724d9ef02df867a46a8b9eee065c0e6b42d33
divisia <omrfyyz@gmail.com>
2020-05-24 15:39:17 +0300

    implement filtering options

--- event/views.py
+++ event/views.py
@@ -10,22 +10,37 @@ class EventViewSet(viewsets.ModelViewSet):
     API endpoint
     """
     serializer_class = EventSerializer
-    queryset = Event.objects.filter(published=True).order_by('-added_at')
+    queryset = Event.objects.filter(published=True)
     
     def get_queryset(self):
         params = self.request.query_params
         print("request params", params)
 
+        # filter: by event type
         etype = params.get('etype', None)
         if etype:
-            print("filtering for event type", etype)
-            etype = EventType.objects.get(name=etype)
-            self.queryset = self.queryset.filter(etype=etype)
+            try:
+                etype = EventType.objects.get(name=etype)
+                self.queryset = self.queryset.filter(etype=etype)
+            except EventType.DoesNotExist:
+                # todo: make below line more elegant
+                self.queryset = self.queryset[:0]
         
-        after = params.get('after', None)
-        if after:
-            print("selecting events after", after)
-            self.queryset = self.queryset.where('starts_at__grea', after)
+        # filter: by location
+        location = params.get('location', None)
+        if location:
+            self.queryset = self.queryset.filter(location=location)
+        
+        # order: by parameter
+        order_by = params.get('order_by', None)
+        if order_by:
+            try:
+                self.queryset = self.queryset.order_by(order_by)
+            except self.FieldError:
+                # todo: make below line more elegant
+                self.queryset = self.queryset[:0]
+        else:
+            self.queryset = self.queryset.order_by('-added_at')
 
         return self.queryset
 
