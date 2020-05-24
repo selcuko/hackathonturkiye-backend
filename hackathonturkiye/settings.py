0462724401339636ee48789a12d2331678a70814
divisia <omrfyyz@gmail.com>
2020-05-25 01:48:38 +0300

    set debug to false

--- hackathonturkiye/settings.py
+++ hackathonturkiye/settings.py
@@ -6,7 +6,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = os.getenv("SECRET_KEY", str(uuid.uuid4()))
 
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost',                        # Gunicorn needs this
