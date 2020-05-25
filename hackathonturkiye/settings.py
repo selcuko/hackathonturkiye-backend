be9ebf0af07c2ef830075e5cf9d5b8d54e68a13d
divisia <omrfyyz@gmail.com>
2020-05-25 22:22:23 +0300

    hotfix: typo caused whole auth system down

--- hackathonturkiye/settings.py
+++ hackathonturkiye/settings.py
@@ -24,8 +24,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 10,
 
-    'DEFAULT_PERMISSON_CLASSES': [
-        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAdminOrReadOnly',
     ]
 }
 
