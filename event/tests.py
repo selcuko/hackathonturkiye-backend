dca277b3607f2326c31a3744af96618fe0942d87
divisia <omrfyyz@gmail.com>
2020-05-25 14:02:21 +0300

    styled admin panel a bit

--- event/tests.py
+++ event/tests.py
@@ -1,10 +1,29 @@
+import json
 from django.test import TestCase
-from django.utils import timezone
-from rest_framework.test import APIRequestFactory, APIClient
+from rest_framework.test import APIRequestFactory
+from event.views import EventViewSet
 
 
-#factory = APIRequestFactory()
-#request = factory.get('/')
+factory = APIRequestFactory()
+view = EventViewSet.as_view({'get':'list'})
 
-#client = APIClient()
-#client.get('/')
+class Methods(TestCase):
+
+    def test_post(self):
+        request = factory.post('events/', {
+            'name':'Lorem ipsum hackathon',
+            'starts_at':'2020-01-02',
+            'origin_url':'https://hackathonturkiye.com',
+            'description':'Test test test test',
+            'location':'online'
+        })
+        response = view(request)
+        TestCase.assertEqual(response.status_code, 200)
+
+
+    def test_get(self):
+        request = factory.get('events/?location=online&format=json')
+        response = view(request)
+        response.render()
+        content = json.loads(response.content)
+        TestCase.assertEqual(content['results'][0]['location'], 'online')
