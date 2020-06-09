f96690eee1e10e25ceaea78b698a8efb878c7cbb
divisia <omrfyyz@gmail.com>
2020-06-09 17:49:35 +0300

    init common app and common tag

---
+++ common/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from djang.utils.text import slugify
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(editable=False, unique=True)
+    description = models.TextField(null=True, editable=False, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name, allow_unicode=False)
+        super().save(*args, **kwargs)
