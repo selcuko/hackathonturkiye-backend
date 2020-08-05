a18d5c1ba6ca8d3e5ef209b92ef7e71893ac690e
divisia <omrfyyz@gmail.com>
2020-08-05 11:08:23 +0300

    img validation

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,32 @@ from django.contrib.auth.models import User
 from djrichtextfield.models import RichTextField
 from common.models import Tag
 
+valid_img_res = [
+    (640, 360),
+    (854, 480),
+    (960, 540),
+    (1024, 576),
+    (1280, 720),
+    (1280, 728),
+]
+w_allowance_percent = .05 # max 1
+h_allowance_percent = .05 # max 1
+
+img_max_size = 256 # kilobytes
+
+def validate_image_res(img):
+    if img.height > img.width:
+        raise ValidationError(f'Resmin boyu eninden büyük. 16:9 oranında resim gerekli.')
+
+    if img.size//1024 > img_max_size:
+        raise ValidationError(f'Resmin dosya boyutu çok büyük. Maximum boyut: {img_max_size}KB')
+
+    for w,h in valid_img_res:
+        if abs(img.width - w) < w*w_allowance_percent and abs(img.height - h) < h*h_allowance_percent:
+            return
+            
+    raise ValidationError(f'Resim istediğim boyutta değil. Geçerli boyutlar: {valid_img_res}')
+
 class PostCategory(models.Model):
     name = models.CharField(max_length=200)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -39,7 +65,11 @@ class Post(models.Model):
         max_length=1400,
         unique=True,
     )
-    thumbnail = models.ImageField(upload_to='blog/thumbnails', default='blog/none.png', max_length=1024)
+    thumbnail = models.ImageField(
+        upload_to='blog/thumbnails', 
+        default='blog/none.png',
+        max_length=1024,
+        validators=[validate_img_res])
 
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True)
--- event/models.py
+++ event/models.py
@@ -1,11 +1,39 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 from datetime import datetime
+from PIL import Image
 from djrichtextfield.models import RichTextField
 from django.utils.text import slugify
 from django.core.exceptions import MultipleObjectsReturned
 from common.models import Tag
+
+valid_img_res = [
+    (640, 360),
+    (854, 480),
+    (960, 540),
+    (1024, 576),
+    (1280, 720),
+    (1280, 728),
+]
+w_allowance_percent = .05 # max 1
+h_allowance_percent = .05 # max 1
+
+img_max_size = 256 # kilobytes
+
+def validate_image_res(img):
+    if img.height > img.width:
+        raise ValidationError(f'Resmin boyu eninden büyük. 16:9 oranında resim gerekli.')
+
+    if img.size//1024 > img_max_size:
+        raise ValidationError(f'Resmin dosya boyutu çok büyük. Maximum boyut: {img_max_size}KB')
+
+    for w,h in valid_img_res:
+        if abs(img.width - w) < w*w_allowance_percent and abs(img.height - h) < h*h_allowance_percent:
+            return
+            
+    raise ValidationError(f'Resim istediğim boyutta değil. Geçerli boyutlar: {valid_img_res}')
 
 
 class EventType(models.Model):
@@ -41,7 +69,8 @@ class Event(models.Model):
     thumbnail = models.ImageField(
         upload_to='thumbnails', 
         default='thumbnails/none/placeholder.jpg',
-        max_length=1024)
+        max_length=1024,
+        validators=[validate_image_res])
     starts_at = models.DateTimeField()
     ends_at = models.DateTimeField(blank=True, null=True)
     deadline = models.DateTimeField(blank=True, null=True)
