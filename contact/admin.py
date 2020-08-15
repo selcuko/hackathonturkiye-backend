744df36d6d0621578a5fa4ab0e9807dfbdca1d9e
divisia <omrfyyz@gmail.com>
2020-08-15 11:12:12 +0300

    unstable mods

--- contact/admin.py
+++ contact/admin.py
@@ -2,5 +2,17 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(ContactForm)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = [
+        'contact',
+        'category__name',
+        'email',
+    ]
+    list_filter = ['category__name']
+    readonly_fields = '__all__'
+    exclude = ['title', 'user_agent', 'remote_addr', 'path']
+
+
+
+admin.site.register(ContactForm, ContactAdmin)
 admin.site.register(ContactFormCategory)
